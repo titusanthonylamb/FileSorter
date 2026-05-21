61a3b5b151f82ed0bc5ab69be3f0ff62cf9bb10c
Titus Lamb <titusanthonylamb@gmail.com>
2026-05-21 15:21:24 -0500

    feat: organize files

--- sort.py
+++ sort.py
@@ -1,10 +1,10 @@
 import os
-import platform
 import shutil
 
 filesDir = input("Unorganized file directory: ")
 outputDir = input("Output directory: ")
 
+# Check if both directories exist
 def directories_exist(files, output):
     if not os.path.exists(files) or not os.path.isdir(files):
         print("File directory does not exist!")
@@ -14,12 +14,34 @@ def directories_exist(files, output):
         return False
     else:
         return True
-    
+
+# Organize files by file type
 def organize_files(files, output):
     with os.scandir(files) as entries:
         for entry in entries:
             if entry.is_file():
-                print(entry.name)
+                sub_output = os.path.splitext(entry)[-1]
+                entry_path = os.path.abspath(entry)
+                output_path = output + "/" + sub_output
+
+                # Create path if path does not exist
+                os.makedirs(output_path, exist_ok = True)
+
+                # Move file to output
+                shutil.move(entry_path, output_path + "/" + entry.name)
+            elif os.path.isdir(entry):
+                entry_path = os.path.abspath(entry)
+                output_path = output + "/folders"
+
+                # Create path if path does not exist
+                os.makedirs(output_path, exist_ok = True)
+
+                # Move file to output
+                shutil.move(entry_path, output_path + "/" + entry.name)
 
 if directories_exist(filesDir, outputDir) == True:
     organize_files(filesDir, outputDir)
+    print("Files successfully moved!")
+
+# Prevent file from closing
+input()
